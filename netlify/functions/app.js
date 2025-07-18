ad718042b994cb76e35a2cbabc24eb46520bd147
AniketKumar5155 <your_email@example.com>
2025-07-19 02:44:12 +0530

    fix: fixed imports

--- netlify/functions/app.js
+++ netlify/functions/app.js
@@ -1,37 +1,39 @@
-import express from "express";
-import serverless from "serverless-http";
-import helmet from "helmet";
-import morgan from "morgan";
-import cors from "cors";
-import cookieParser from "cookie-parser";
-import dotenv from "dotenv";
-import notFoundMiddleware from "../../middlewares/notFoundMiddleware.js";
-import errorHandlerMiddleware from "../../middlewares/errorHandlerMiddleware.js";
-// import rateLimit from "express-rate-limit";
-import authRouter from "../../routes/authRoutes.js";
-import otpRouter from "../../routes/otpRoutes.js";
-import noteRouter from "../../routes/noteRoutes.js";
+const express = require("express");
+const serverless = require("serverless-http");
+const helmet = require("helmet");
+const morgan = require("morgan");
+const cors = require("cors");
+const cookieParser = require("cookie-parser");
+const dotenv = require("dotenv");
+
+const notFoundMiddleware = require("../../middlewares/notFoundMiddleware.js");
+const errorHandlerMiddleware = require("../../middlewares/errorHandlerMiddleware.js");
+// const rateLimit = require("express-rate-limit");
+
+const authRouter = require("../../routes/authRoutes.js");
+const otpRouter = require("../../routes/otpRoutes.js");
+const noteRouter = require("../../routes/noteRoutes.js");
 
 dotenv.config();
+
 const app = express();
 
 app.use(helmet());
 app.use(morgan("dev"));
 
-app.use(cors())
+app.use(cors());
 app.use(express.json());
 app.use(cookieParser());
 // app.use(rateLimit); 
 
 app.use("/auth", authRouter);
 app.use("/otp", otpRouter);
-app.use('/note', noteRouter)
-
+app.use("/note", noteRouter);
 
 app.use(notFoundMiddleware);
 app.use(errorHandlerMiddleware);
 
-const PORT = process.env.PORT || 3000;
+// const PORT = process.env.PORT || 3000;
 // app.listen(PORT, () => {
 //   console.log(`Server is running on http://localhost:${PORT}`);
 // });
@@ -40,4 +42,4 @@ const PORT = process.env.PORT || 3000;
 //   console.log("Server running on http://<your-local-ip>:3000");
 // });
 
-export const handler = serverless(app);
+module.exports.handler = serverless(app);
