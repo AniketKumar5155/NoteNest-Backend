70ff44769ee82caedc493f7769346008abe62fca
AniketKumar5155 <your_email@example.com>
2025-07-19 03:18:17 +0530

    feat: updated to common js

--- config/config.cjs
+++ config/config.cjs
@@ -1,11 +1,18 @@
 require('dotenv').config();
 
+const dbUrl = process.env.NETLIFY_DATABASE_URL;
+
 module.exports = {
-  development: {
-    username: process.env.DB_USER || "root",
-    password: process.env.DB_PASS|| null,
-    database: process.env.DB_NAME || "your_db_name",
-    host: process.env.DB_HOST || "127.0.0.1",
-    dialect: "mysql"
-  },
+  development: dbUrl
+    ? {
+        use_env_variable: 'NETLIFY_DATABASE_URL',
+        dialect: 'postgres',
+      }
+    : {
+        username: process.env.DB_USER || "root",
+        password: process.env.DB_PASS || null,
+        database: process.env.DB_NAME || "your_db_name",
+        host: process.env.DB_HOST || "127.0.0.1",
+        dialect: "mysql",
+      },
 };
