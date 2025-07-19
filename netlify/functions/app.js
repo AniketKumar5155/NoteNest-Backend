920c899b3debae81f85335bb4f1849dd7d5c9fcf
AniketKumar5155 <your_email@example.com>
2025-07-19 14:38:22 +0530

    feat: removed user_id from otp model

--- netlify/functions/app.js
+++ netlify/functions/app.js
@@ -8,7 +8,6 @@ const dotenv = require("dotenv");
 
 const notFoundMiddleware = require("../../middlewares/notFoundMiddleware.js");
 const errorHandlerMiddleware = require("../../middlewares/errorHandlerMiddleware.js");
-// const rateLimit = require("express-rate-limit");
 
 const authRouter = require("../../routes/authRoutes.js");
 const otpRouter = require("../../routes/otpRoutes.js");
@@ -17,6 +16,12 @@ const noteRouter = require("../../routes/noteRoutes.js");
 dotenv.config();
 const app = express();
 
+app.use(helmet());
+app.use(morgan("dev"));
+app.use(cors());
+app.use(express.json()); // ✅ Must come first
+
+// ✅ Fix Netlify string body issue
 app.use((req, res, next) => {
   if (req.body && typeof req.body === "string") {
     try {
@@ -28,12 +33,7 @@ app.use((req, res, next) => {
   next();
 });
 
-app.use(helmet());
-app.use(morgan("dev"));
-app.use(cors());
-app.use(express.json());
 app.use(cookieParser());
-// app.use(rateLimit);
 
 app.use("/auth", authRouter);
 app.use("/otp", otpRouter);
@@ -41,14 +41,5 @@ app.use("/note", noteRouter);
 
 app.use(notFoundMiddleware);
 app.use(errorHandlerMiddleware);
-
-// const PORT = process.env.PORT || 3000;
-// app.listen(PORT, () => {
-//   console.log(`Server is running on http://localhost:${PORT}`);
-// });
-
-// app.listen(3000, '0.0.0.0', () => {
-//   console.log("Server running on http://<your-local-ip>:3000");
-// });
 
 module.exports.handler = serverless(app);
