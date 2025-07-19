50509219c9cd55a2db5f5e0b9a4870cbccb820f1
AniketKumar5155 <your_email@example.com>
2025-07-19 15:08:24 +0530

    chore: added mysql2 pakage

--- config/config.cjs
+++ config/config.cjs
@@ -1,24 +1,23 @@
 require('dotenv').config();
 
-const isProd = process.env.NODE_ENV === 'production';
-
-module.exports = isProd
-  ? {
-      use_env_variable: 'NETLIFY_DATABASE_URL',
-      dialect: 'postgres',
-      dialectModule: require('pg'),
-      dialectOptions: {
-        ssl: {
-          require: true,
-          rejectUnauthorized: false,
-        },
+module.exports = {
+  development: {
+    username: process.env.DB_USER || "root",
+    password: process.env.DB_PASS || null,
+    database: process.env.DB_NAME || "your_db_name",
+    host: process.env.DB_HOST || "127.0.0.1",
+    dialect: "mysql",
+    dialectModule: require('mysql2'),
+  },
+  production: {
+    use_env_variable: 'NETLIFY_DATABASE_URL',
+    dialect: 'postgres',
+    dialectModule: require('pg'),
+    dialectOptions: {
+      ssl: {
+        require: true,
+        rejectUnauthorized: false,
       },
-    }
-  : {
-      username: process.env.DB_USER || "root",
-      password: process.env.DB_PASS || null,
-      database: process.env.DB_NAME || "your_db_name",
-      host: process.env.DB_HOST || "127.0.0.1",
-      dialect: "mysql",
-      dialectModule: require('mysql2'),
-    };
+    },
+  },
+};
